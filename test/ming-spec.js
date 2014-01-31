0e8870fddb763e880eb24cf59aa54162d8f72753
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-01-31 10:42:57 -0600

    Add register tests.

--- test/ming-spec.js
+++ test/ming-spec.js
@@ -19,4 +19,35 @@ describe("Ming", function () {
         });
     });
 
+    describe("register", function () {
+
+        it("should throw an error if username or password are not given", function (done) {
+            ming.register({}, function (err) {
+                expect(err).to.not.be(null);
+                ming.register({
+                    username: "ming"
+                }, function (err) {
+                    expect(err).to.not.be(null);
+                    ming.register({
+                        password: "ming"
+                    }, function (err) {
+                        expect(err).to.not.be(null);
+                        done();
+                    });
+                });
+            });
+        });
+
+        it("should register a user", function (done) {
+            ming.register({
+                username: "ming",
+                password: "ming"
+            }, function (err, id) {
+                expect(err).to.be(null);
+                done();
+            });
+        });
+
+    });
+
 });
